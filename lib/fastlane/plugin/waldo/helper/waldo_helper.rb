03594ba1da0c773c10ce35ea1c1bfe59e9c99b5b
John Pusey <john@waldo.io>
2022-04-13 18:22:14 -0400

    Decoupled Waldo Go CLI

--- lib/fastlane/plugin/waldo/helper/waldo_helper.rb
+++ lib/fastlane/plugin/waldo/helper/waldo_helper.rb
@@ -1,6 +1,74 @@
 module Fastlane
   module Helper
     class WaldoHelper
+      require 'net/http'
+
+      def self.determine_asset_name
+        platform = RUBY_PLATFORM.downcase
+
+        if platform.include?('linux')
+          ext = ''
+          os = 'linux'
+        elsif platform.include?('darwin')
+          ext = ''
+          os = 'macos'
+        elsif platform.include?('mswin')
+          ext = '.exe'
+          os = 'windows'
+        else
+          UI.error("Unsupported platform: #{platform}")
+        end
+
+        if platform.include?('arm64')
+          arch = 'arm64'
+        elsif platform.include?('x86_64')
+          arch = 'x86_64'
+        else
+          UI.error("Unsupported platform: #{platform}")
+        end
+
+        "waldo-#{os}-#{arch}#{ext}"
+      end
+
+      def self.download(uri, path)
+        begin
+          request_uri = uri.request_uri
+          response = nil
+
+          loop do
+            response = Net::HTTP.get_response(uri)
+
+            break unless response.is_a?(Net::HTTPRedirection)
+
+            uri = URI.parse(response['location'])
+          end
+
+          fp = File.open(path, 'wb')
+
+          fp.write(response.body)
+        rescue => exc
+          UI.error("Unable to download #{request_uri}: #{exc.inspect.to_s}")
+        ensure
+          fp.close if fp
+        end
+      end
+
+      def self.download_binary
+        asset_name = determine_asset_name
+
+        uri_string = 'https://github.com/waldoapp/waldo-go-cli/releases/latest/download/'
+
+        uri_string += asset_name
+
+        binary_path = File.join(Dir.tmpdir, 'waldo')
+
+        download(URI(uri_string), binary_path)
+
+        File.chmod(0755, binary_path)
+
+        binary_path
+      end
+
       def self.filter_parameters(in_params)
         out_params = {}
 
@@ -12,9 +80,9 @@ module Fastlane
         upload_token = in_params[:upload_token]
         variant_name = in_params[:variant_name] || Actions.lane_context[Actions::SharedValues::GRADLE_BUILD_TYPE]
 
-        apk_path.gsub!("\\ ", ' ') if apk_path
-        app_path.gsub!("\\ ", ' ') if app_path
-        ipa_path.gsub!("\\ ", ' ') if ipa_path
+        apk_path.gsub!('\\ ', ' ') if apk_path
+        app_path.gsub!('\\ ', ' ') if app_path
+        ipa_path.gsub!('\\ ', ' ') if ipa_path
 
         out_params[:apk_path] = apk_path if apk_path
 
@@ -49,35 +117,6 @@ module Fastlane
         out_params
       end
 
-      def self.get_binary_path
-        platform = RUBY_PLATFORM.downcase
-
-        if platform.include?("linux")
-          ext = ""
-          os = "linux"
-        elsif platform.include?("darwin")
-          ext = ""
-          os = "macos"
-        elsif platform.include?("mswin")
-          ext = ".exe"
-          os = "windows"
-        else
-          UI.error("Unsupported platform: #{platform}")
-        end
-
-        if platform.include?("arm64")
-          arch = "arm64"
-        elsif platform.include?("x86_64")
-          arch = "x86_64"
-        else
-          UI.error("Unsupported platform: #{platform}")
-        end
-
-        root = Pathname.new(File.expand_path('../../..', __FILE__))
-
-        File.join(root, "waldo", "assets", "waldo-#{os}-#{arch}#{ext}")
-      end
-
       def self.upload_build(params)
         apk_path = params[:apk_path]
         app_path = params[:app_path]
@@ -90,7 +129,7 @@ module Fastlane
         elsif ipa_path
           build_path = ipa_path
         else
-          build_path = ""
+          build_path = ''
         end
 
         command = []
@@ -98,8 +137,8 @@ module Fastlane
         command << "WALDO_WRAPPER_NAME_OVERRIDE='Fastlane'"
         command << "WALDO_WRAPPER_VERSION_OVERRIDE='#{Fastlane::Waldo::VERSION}'"
 
-        command << get_binary_path.shellescape
-        command << "upload"
+        command << download_binary.shellescape
+        command << 'upload'
 
         if params[:git_branch]
           command << '--git_branch'
--- lib/fastlane/plugin/waldo/version.rb
+++ lib/fastlane/plugin/waldo/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
   module Waldo
-    VERSION = "3.0.1"
+    VERSION = "3.1.0"
   end
 end
